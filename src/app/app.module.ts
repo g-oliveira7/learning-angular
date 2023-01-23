9b634d1f07944be53297aba0c05dbb900b35906c
g-oliveira7 <studying.online@outlook.com>
2023-01-23 13:18:58 -0300

    Creating modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CursosModule } from './cursos/cursos.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,7 +11,8 @@ import { FirstComponent } from './first/first.component';
     FirstComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
