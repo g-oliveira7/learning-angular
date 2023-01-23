a756a64ddef918c5d3627348161447617676dd3c
g-oliveira7 <studying.online@outlook.com>
2023-01-23 15:32:49 -0300

    Event binding

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,5 +1,6 @@
 import { CursosService } from './cursos.service';
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-cursos',
@@ -11,9 +12,16 @@ export class CursosComponent {
   cursos: string[];
   tiposAlert: string[];
 
-  constructor(private cursosService: CursosService) {
+  constructor(private cursosService: CursosService,
+    private modalService: NgbModal) {
     this.nomePortal = 'http://loiane.training'
     this.cursos = this.cursosService.getCursos()
     this.tiposAlert = ['success', 'info', 'warning', 'danger', 'primary', 'secondary', 'light', 'dark']
+  }
+
+  handleClick(content: any) {
+    this.modalService.open(content, {
+      backdrop: 'static'
+    })
   }
 }
