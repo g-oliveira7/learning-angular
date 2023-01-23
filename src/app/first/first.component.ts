d364ca2d080d8ce8c75fb73c95ff37b0e0a84cce
g-oliveira7 <studying.online@outlook.com>
2023-01-23 12:55:47 -0300

    First component

---
+++ src/app/first/first.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'first-component',
+    template: `
+        <p> First component with Angular!</p>
+    `
+})
+export class FirstComponent { }
